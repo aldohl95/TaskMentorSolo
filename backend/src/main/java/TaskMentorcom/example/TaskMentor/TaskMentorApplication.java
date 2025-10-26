d80e47431095aa132f184819dd95488d13c71909
Tevita Mathias <mathias.tevita@icloud.com>
2025-10-25 17:36:26 -0700

    setup frontend and backend

---
+++ backend/src/main/java/TaskMentorcom/example/TaskMentor/TaskMentorApplication.java
@@ -0,0 +1,13 @@
+package TaskMentorcom.example.TaskMentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMentorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/TaskMentorcom/example/TaskMentor/TaskMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package TaskMentorcom.example.TaskMentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
