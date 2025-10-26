2063cad3de6a36f0d7f38bb3a68c5bf8e01c9625
Tevita Mathias <mathias.tevita@icloud.com>
2025-10-26 15:29:34 -0700

    Basic folder structure created, User class and AccountType class setup

---
+++ backend/taskmentor/src/main/java/com/taskmentor/taskmentor/TaskmentorApplication.java
@@ -0,0 +1,13 @@
+package com.taskmentor.taskmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmentorApplication.class, args);
+	}
+
+}
---
+++ backend/taskmentor/src/test/java/com/taskmentor/taskmentor/TaskmentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmentor.taskmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
